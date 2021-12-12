372ad7aa0e45842910f777b31d2cddb82c2c33e2
Soo Young Kim (Swimming Kim) <tge1375@naver.com>
2021-12-12 13:53:44 +0900

    Create ros_melodic_install.sh

---
+++ ros_melodic_install.sh
@@ -0,0 +1,20 @@
+sudo apt update
+sudo apt-get install curl
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update
+sudo apt install ros-melodic-desktop-full -y
+
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo rosdep init
+rosdep update
+
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+catkin_init_workspace
+cd ../
+catkin_make
